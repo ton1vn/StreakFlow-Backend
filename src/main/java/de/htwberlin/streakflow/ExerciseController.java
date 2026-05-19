89caf2e90eea3028868815efb3b25c98bb3021f8
ton1vn <toninguyen206@gmail.com>
2026-05-20 01:49:01 +0200

    Enable CORS for frontend

--- src/main/java/de/htwberlin/streakflow/ExerciseController.java
+++ src/main/java/de/htwberlin/streakflow/ExerciseController.java
@@ -1,10 +1,12 @@
 package de.htwberlin.streakflow;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ExerciseController {
 
