0c01a52d1245ee1c0f7f4a66f5498e5694d2e930
ton1vn <157405586+ton1vn@users.noreply.github.com>
2026-06-02 11:00:20 +0200

    Persist exercises as database entities

--- src/main/java/de/htwberlin/streakflow/Exercise.java
+++ src/main/java/de/htwberlin/streakflow/Exercise.java
@@ -1,11 +1,22 @@
 package de.htwberlin.streakflow;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Exercise {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String category;
     private int targetMinutesPerDay;
+
+    public Exercise() {
+    }
 
     public Exercise(Long id, String name, String category, int targetMinutesPerDay) {
         this.id = id;
@@ -18,15 +29,31 @@ public class Exercise {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getCategory() {
         return category;
     }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public int getTargetMinutesPerDay() {
         return targetMinutesPerDay;
+    }
+
+    public void setTargetMinutesPerDay(int targetMinutesPerDay) {
+        this.targetMinutesPerDay = targetMinutesPerDay;
     }
 }
